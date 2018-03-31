0b0832701f5a5a8772c28141bc4e7c9656371303
Li Zhaoyang <lizy14@yeah.net>
2018-03-31 14:59:32 +0800

    process lists for downloading imagenet synsets

--- process-lists.py
+++ process-lists.py
@@ -1,41 +1,43 @@
-import os
-import subprocess
-
-imagenet_root = "/home/dataset/ILSVRC2015/Data/CLS-LOC/train/"
-
-filelist = "/home/justin/o/yfcc100m-entity/data/{domain}/full.txt"
+filelist = "/home/justin/s/datasets/yfcc100m-entity/data/{domain}/{which_set}.txt"
 
 expected_url_prefix = "https://multimedia-commons.s3-us-west-2.amazonaws.com/data/images/"
 
-urls_filename = "urls.txt"
+urls_filename = "urls-{domain}-{which_set}.txt"
 
-with open(urls_filename, "w") as of:
+synsets_filename = "synsets-{domain}-{which_set}.txt"
+
+def main():
     for domain in ['Artifacts', 'Species-I', 'Species-Y', 'Sports']:
-        if domain != 'Sports':
-            continue
-        total = 0
-        exist = [0, 0, 0]
-        with open(filelist.format_map({'domain': domain})) as f:
-            for line in f:
-                filename, label, source = line.strip().split(" ")
-                total += 1
-                if source == "imagenet":
-                    if os.path.exists(os.path.join(imagenet_root, filename)):
-                        exist[0] += 1
-                    else:
-                        exist[1] += 1
-                        pass # print("not found in imagenet: ", filename)
-                    # TODO: 
-                elif source == "yfcc100m":
-                    splits = filename.split('/')
-                    expected_filename_prefix = (splits[-3] + splits[-2])
-                    if filename.startswith(expected_url_prefix) and splits[-1].startswith(expected_filename_prefix):
-                        exist[2] += 1
-                        print(filename, file=of)
-                    else:   
-                        print("unexpected url: ", filename)
-                else:
-                    print("unknown source: ", source)
-                continue
+        for which_set in ['dev', 'full', 'test']:
+            with open(urls_filename.format_map({'domain': domain, 'which_set': which_set}), "w") as yfccof:
+                with open(synsets_filename.format_map({'domain': domain, 'which_set': which_set}), "w") as imagenetof:
+                
+                    total = 0
+                    exist = [0, 0]
+                    with open(filelist.format_map({'domain': domain, 'which_set': which_set})) as f:
+                        for line in f:
+                            filename, label, source = line.strip().split(" ")
+                            total += 1
+                            if source == "imagenet":
+                                    exist[0] += 1
+                                    actual_filename = filename.split('/')[-1].split('.')[0]
+                                    # ImageNet Synset
+                                    print(actual_filename.split('_')[0], file=imagenetof)
 
-        print(domain, ':' , exist, "/", total)
+                            elif source == "yfcc100m":
+                                splits = filename.split('/')
+                                expected_filename_prefix = (splits[-3] + splits[-2])
+                                if filename.startswith(expected_url_prefix) and splits[-1].startswith(expected_filename_prefix):
+                                    exist[1] += 1
+                                    # YFCC100M url
+                                    print(filename, file=yfccof)
+                                else:
+                                    print("unexpected url: ", filename)
+                            else:
+                                print("unknown source: ", source)
+                            continue
+
+            print(domain, which_set, ':' , exist, "/", total)
+
+if __name__ == "__main__":
+    main()
